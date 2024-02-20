b2d9779df6a018125a63757b4d9f5466d6c0dfbb
Tsering Dhargyal <dhargyalofficial@gmail.com>
2024-02-20 16:10:27 +0000

    Merge changes from remote repository

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-#If the bill was $150.00, split between 5 people, with 12% tip. 
+#If the bill was $150.00, split between 5 people, with 12% tip.
 
 #Each person should pay (150.00 / 5) * 1.12 = 33.6
 #Format the result to 2 decimal places = 33.60
@@ -10,5 +10,6 @@ print("Welcome to the tip calculator!")
 bill = input("What was the total bill? $")
 tip = input("How much tip would you like to give? 10, 12, or 15? ")
 people = input("How many people to split the bill? ")
-total_bill = round((float(bill) / int(people)) * (1 + (int(tip) / 100)),2)
+# there are also other ways to do this
+total_bill = round((float(bill) / int(people)) * (1 + (int(tip) / 100)), 2)
 print(f"Each person should pay: ${total_bill}")
--- replit.nix
+++ replit.nix
@@ -1,5 +1,6 @@
 { pkgs }: {
   deps = [
+    pkgs.openssh
     pkgs.python38Full
   ];
   env = {
